834201b55c39d194575cfc4076465c0e19563fb4
HAYAMA Kaoru <iyahaya@nifty.com>
2007-11-24 15:17:53 +0900

    RCS-revision 1.35 date: 2007/11/24 15:17:53;  author: hayama;  state: Exp;  lines: +2 -1 * License .

--- wifky.dat/nikky.pl
+++ wifky.dat/nikky.pl
@@ -1,5 +1,6 @@
 # nikky.pl - diary plugin for wifky.pl
 # 2006-2007 (c) NoGood/VOYAGER/Hayamadas
+# Licensed with Modified BSD Style.
 
 package wifky::nikky;
 
@@ -55,7 +56,7 @@ if( exists $::form{a} && ($::form{a} eq 'date' || $::form{a} eq 'nikky') ){
     delete $::menubar{'400_Edit(Admin)'};
 }
 
-$::preferences{'Plugin: nikky.pl '.$version.' $Date: 2007/11/24 15:02:19 $'}= [
+$::preferences{'Plugin: nikky.pl '.$version.' $Date: 2007/11/24 15:17:53 $'}= [
     { desc=>'Author'
     , name=>'nikky_author' , size=>20 },
     { desc=>'Print diary as FrontPage'
