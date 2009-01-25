95f413f8b73d883ef92e4b2817aafe242865d2d8
HAYAMA Kaoru <iyahaya@nifty.com>
2009-01-26 01:57:30 +0900

    * confidential plugin (add monta method)

--- wifky.d/confidential.pl
+++ wifky.d/confidential.pl
@@ -1,8 +1,18 @@
+$::inline_plugin{monta} = sub{
+    if( $::form{a} !~ /rss/ ){
+        '<span style="color:gray;background-color:gray">'.
+        $_[0]->{argv}.
+        '</span>';
+    }else{
+        '*' x length($_[0]->{argv});
+    }
+};
+
 $::inline_plugin{confidential} = sub{
     if( ! &::is_frozen() ){
         '<blink>Please freeze this page to hide the secret information.</blink>'
     }elsif( &::is_signed() ){
-	$_[0]->{argv};
+        goto &{$::inline_plugin{monta}};
     }else{
         &anchor( 
             '(Confidential)',
