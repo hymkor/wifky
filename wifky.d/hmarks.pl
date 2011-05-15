e6e722a2f8ca456d6f978b31371027e7d464b134
HAYAMA Kaoru <iyahaya@nifty.com>
2011-05-16 00:07:57 +0900

    * hmarks: (twitter)EUC-JP環境でも動作するようにした。 * 新スタイルのはてなブックマークに対応

--- wifky.d/hmarks.pl
+++ wifky.d/hmarks.pl
@@ -1,31 +1,26 @@
 package wifky::hmarks;
-use strict;use warnings;
-use Encode ();
-
-my $utf8=Encode::find_encoding('utf8');
-my $eucjp=Encode::find_encoding('euc-jp');
+#use strict;use warnings;
 
 my $version="1.6_0";
 
-###
-### Hatena Bookmark Secion ###
-###
-
-if( exists $::form{utf8p} ){
+if( exists $::form{hp} ){
     print  "Status: 301 See Other\r\n";
-    printf "Location: %s\r\n",
-        &::myurl( { p=>$eucjp->encode( $utf8->decode( $::form{utf8p} ) ) } );
+    printf "Location: %s\r\n", &::myurl( { p=>pack('h*',$::form{hp} ) } );
     print  "\r\n\r\n";
     exit(0);
 }
 
-if( ! exists $::form{p} && exists $::form{q} ){
-    $::form{p} = unypack($::form{q});
+if( ! exists $::form{p} && exists $::form{hp} ){
+    $::form{p} = pack('h*',$::form{hp});
 }
 
 if( $::config{hmark_each_section} ){
     (*::midashi,*org_midashi ) = (*new_midashi,*::midashi);
 }
+
+###
+### Hatena Bookmark Secion ###
+###
 
 sub new_midashi{
     my ($depth,$text,$session)=@_;
@@ -73,55 +68,39 @@ sub marking{
                   : $ENV{'SERVER_NAME'}
             ) . $ENV{'SCRIPT_NAME'};
     local $::postme=$::me;
-    my $url1 = &::myurl( { p=>$session->{title} } , $sharp||'' );
-    (my $url2 = $url1)=~s/\#/\%23/g;
+    my $bookmark_url = &::myurl( { p=>$session->{title} } , $sharp||'' );
+    (my $bookmark_entry_url = $bookmark_url)=~s/\#/\%23/g;
 
     $title ||= $session->{title};
     $title =~ s/^ +//;
     my $fulltitle = $::config{sitename} . ' - ' . $title;
 
+    my $tweet_url;
     if( $::charset eq 'EUC-JP' ){
-        $url1 = &::myurl(
-            { utf8p=>$utf8->encode( $eucjp->decode($session->{title}) ) } ,
-            $sharp||''
-        );
+        $tweet_url = &::myurl( { hp=>unpack('h*',$session->{title}) } , $sharp||'');
+    }else{
+        $tweet_url = $bookmark_url;
     }
-    $url1 =~ s/\+/\%20/g;
+    $tweet_url =~ s/\+/\%20/g;
 
     # [Bookmark anchor]
     &::verb( 
-        sprintf(
-            ' <a href="http://b.hatena.ne.jp/add?mode=confirm&title=%s&url=%s"><img src="%s" alt="[B!]" border="0" /></a><a href="http://b.hatena.ne.jp/entry/%s"><img src="http://b.hatena.ne.jp/entry/image/%s" border="0" alt="[n user]"/></a>'
-            , &::percent( $fulltitle )
-            , &::percent( $url1 )
-            , &::enc($::config{hatenabookmark_mark} || ($::me . '?a=b_entry') )
-            , $url2
-            , $url2
-        ) .
+        sprintf('<a href="http://b.hatena.ne.jp/entry/%s" class="hatena-bookmark-button" data-hatena-bookmark-title="%s" data-hatena-bookmark-layout="%s" title="Add this entry to add hatena bookmark"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="Add this entry to add hatena bookmark" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>'
+            , $bookmark_url
+            , &::enc( $fulltitle ) 
+            , $::config{hmark_bookmark_style} || 'standard' ) .
         # [Twitter mark]
         sprintf(
             ' <a href="http://twitter.com/share" class="twitter-share-button" data-url="%s" data-text="&quot;%s&quot;" data-count="%s" %s data-lang="ja">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>'
-            , &::enc($url1)
+            , &::enc($tweet_url)
             , &::enc($fulltitle)
-            , $::config{hmark_tweet_counter} ? 'horizontal' : 'none'
+            , $::config{hmark_tweet_style} || 'none'
             , $::config{hmark_twitter_id} 
                 ? 'data-via="'.&::enc($::config{hmark_twitter_id}).'"'
                 : ''
         )
     );
 }
-
-### Print mark [B!] ###
-$::action_plugin{b_entry} = sub {
-    my $image=pack('h*',<<__BIN__);
-7494648393160100c0001920008114ecffffffffffff000000129f401000002000c2000000000100c00000206241e89961deffe0c0ac94a614bb21d6d418a47558b568278e7668ad562c952ce46306c8ab050000b3
-__BIN__
-    print  "Content-Type: image/gif\n";
-    printf "Content-Length: %d\n",length($image);
-    print  "\n";
-    print  $image;
-    exit(0);
-};
 
 ###
 ### Hatena Star Section
@@ -162,16 +141,11 @@ $::preferences{"Heading marks ${version}"} = [
     { desc=>'Twitter: Your id' , name=>'hmark_twitter_id' },
     { desc=>'Twitter: mark on each page header', name=>'hmark_each_page', type=>'checkbox' },
     { desc=>'Twitter: mark on each section header', name=>'hmark_each_section', type=>'checkbox' },
-    { desc=>'Twitter: display Tweet counter' , name=>'hmark_tweet_counter' , type=>'checkbox' },
+    { desc=>'Twitter: display Tweet counter' , name=>'hmark_tweet_style' , type=>'radio',
+        option=>[['horizontal','horizontal'],['vertical','vertical'],['none','simple']] },
     { desc=>'HatenaStar: Token', name=>'hatenastar_token', type=>'text', size=>41 },
-    { desc=>'HatenaStar: image(URL)',   name=>'hatenastar_star_image' , size=>41 },
-    { desc=>'HatenaStar: Add button image(URL)', name=>'hatenastar_add_button_image', size=>41 },
-    { desc=>'HatenaStar: Comment button image(URL)', name=>'hatenastar_comment_button_image', size=>41 },
-    { desc=>'HatenaBookmark: static-image(URL)' , name=>'hatenabookmark_mark' , size=>41 } ,
+    { desc=>'HatenaBookmark Style', name=>'hmark_bookmark_style', type=>'radio' ,
+        option=>[['standard','horizontal'],['vertical','vertical'],['simple','simple']] } ,
 ];
 
-sub unypack{
-    (my $s=shift) =~ y/\-_A-Za-z0-9/\x20-\x95/;
-    unpack('u*',$s);
-}
 # vim:set sw=4 et notextmode:
