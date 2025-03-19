175790b1e33a8403445a24b592080b1eacc95085
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-03-19 13:28:11 +0900

    wifky.pl: bump to v1.5.13_4

--- wifky.pl
+++ wifky.pl
@@ -5,7 +5,7 @@ BEGIN{
     eval{ require 'warnings.pm'; }; warnings->import() unless $@;
 }
 
-$::version  = '1.5.13_3';
+$::version  = '1.5.13_4';
 $::PROTOCOL = '(?:s?https?|ftp)';
 $::RXURL    = '(?:s?https?|ftp)://[-\\w.!~*\'();/?:@&=+$,%#]+' ;
 $::charset  = 'UTF-8';
